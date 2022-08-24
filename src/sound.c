24e2162044959681e2fb9dd3f8a268d015416c68
comeillfoo <rayveleaveme34@inbox.ru>
2022-08-24 14:27:11 +0300

    fix: redefined timespec problem; feat: removing built files

--- src/sound.c
+++ src/sound.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+#define _POSIX_SOURCE // otherwise there is timespec redefinition error
 #include <alsa/asoundlib.h>
 
 int main(void) {
     char **names;
     int err = snd_device_name_hint(-1, "ctl", (void ***)&names);
+    if (err != 0) {
+        fprintf(stderr, "snd_device_name_hint returned non-zero code\n");
+        return -1;
+    }
     int sz = 0;
     for (char **it = names; *it != NULL; ++it)
         printf("%d: %s\n", ++sz, *it);
