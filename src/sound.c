ff12941c28af0582c1da6659a4e52713628bfe32
comeillfoo <rayveleaveme34@inbox.ru>
2022-08-24 13:37:50 +0300

    feat: removed redundant includes in sound.c; fix: added library dependancy in Makefile.am

--- src/sound.c
+++ src/sound.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-#include <alsa/global.h>
-#include <alsa/input.h>
-#include <alsa/output.h>
-#include <alsa/conf.h>
-#include <alsa/control.h>
 
+#include <alsa/asoundlib.h>
 
 int main(void) {
-	char** names;
-	int err = snd_device_name_hint(-1, "ctl", (void***) &names);
-	int sz = 0;
-	for (char** it = names; *it != NULL; ++it)
-		printf("%d: %s\n", ++sz, *it);
-	return 0;
+    char **names;
+    int err = snd_device_name_hint(-1, "ctl", (void ***)&names);
+    int sz = 0;
+    for (char **it = names; *it != NULL; ++it)
+        printf("%d: %s\n", ++sz, *it);
+    return 0;
 }
